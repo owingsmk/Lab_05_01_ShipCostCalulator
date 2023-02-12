87d7a5c93ae75f519bd6680b8ed430833bcb8216
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 14:53:26 -0500

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        final int freeShipping = 100
         System.out.println("What is the price of your item?"); //Output asking what the price of the item was
         int itemPrice = 89;  //User input of price
         double shippingCost;
         double totalPrice;
 
-        if(itemPrice >= 100);
+        if(itemPrice>=freeShipping);
+        {
             System.out.println("Your shipping is free! ");
-        if(itemPrice < 100);
+        }
+        if (itemPrice<freeShipping);
+        {
             System.out.println("You will be charged 2% shipping fee plus your item price. ");
+        }
             shippingCost = itemPrice * 0.02;
         System.out.println("Your shipping cost will be: "+ shippingCost);
         totalPrice=itemPrice+shippingCost;
