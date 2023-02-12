e02fb4c77b76f6b3a00253c6a3a401eb73672d2a
Mark Owings <owingsmk@mail.uc.edu>
2023-02-12 15:43:43 -0500

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-
-        final int freeShipping = 100
-        System.out.println("What is the price of your item?"); //Output asking what the price of the item was
-        int itemPrice = 89;  //User input of price
+        //First varible
+        final double freeShipping = 100;
         double shippingCost;
         double totalPrice;
+                System.out.println("What is the price of your item?"); //Output asking what the price of the item was
+        //Step 2 gets input
+        int itemPrice = 100;  //User input of price
 
-        if(itemPrice>=freeShipping);
-        {
-            System.out.println("Your shipping is free! ");
-        }
-        if (itemPrice<freeShipping);
-        {
-            System.out.println("You will be charged 2% shipping fee plus your item price. ");
-        }
-            shippingCost = itemPrice * 0.02;
-        System.out.println("Your shipping cost will be: "+ shippingCost);
+        //Step 3 logical opterations
+            if (itemPrice>=freeShipping) {
+                System.out.println("Your shipping is free! ");
+                shippingCost=0;
+            }
+            else {
+                System.out.println("Your will pay a 2% shipping fee. ");
+                shippingCost = itemPrice * 0.02;
+                System.out.println("Your shipping cost will be: "+ shippingCost);
+            }
         totalPrice=itemPrice+shippingCost;
-                System.out.println("Your total plus shipping is " + totalPrice);
-
-    }
+        System.out.println("Your total plus shipping is " + totalPrice);
+     }
 }
